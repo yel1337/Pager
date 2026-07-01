f96a2462cc687fb8b6310da86d006ab2eddb417e
yel1337 <daniel.salvaloza2002@gmail.com>
2026-07-01 22:58:26 +0800

    removed unnecessary functions and made itself out of segmentation fault

--- src/main.c
+++ src/main.c
@@ -8,9 +8,12 @@
 #include "process/pr.h"
 
 #define PAGE_SIZE 0x10000
+#define ADDRESS_SPACE 0x100000
 #define PTE_SIZE 0x4000
 #define P_MEMORY 0x1000 /* A PROCESS NEEDS 4KB */
 #define PARENT_SUCCESS 0
+#define PAGE_NUMBER ADDRESS_SPACE / PAGE_SIZE
+#define OFFSET(VIRTUAL_ADDRESS, PAGE_SIZE) (VIRTUAL_ADDRESS % PAGE_SIZE)
 
 /*
  * Supposed this is the page table intermediary between
@@ -28,9 +31,9 @@ typedef struct {
 	int page_count;
 } PT;
 
-typedef struct{
-	uint32_t *logical_space;
-	uint32_t **pte_frame;
+typedef struct LOGICAL_MEMORY {
+	struct LOGICAL_MEMORY *logical_space;
+	struct LOGICAL_MEMORY **pte_frame;
 } LOGICAL_MEMORY;
 
 /*
@@ -59,144 +62,64 @@ static int parent_check(pid_t *pr)
 	return 0;
 }
 
-static uint32_t *debug_out_frame(const LOGICAL_MEMORY *logical_mem,
-								int frame_index, int debug_flag)
-{
-	/*
-	 * This function can print out page entry's mem address or
-	 * number of page indexes
-	 * 			0 = PE's MEMORY ADDRESS
-	 * 			1 = n OF INDEXES OF A PAGE
-	 */
-	LOGICAL_MEMORY l_mem;
-
-	if (debug_flag == 0) {
-		uint32_t *mem = logical_mem->pte_frame[frame_index];
-		return mem;
-	} else if (debug_flag == 1) {
-		return (uint32_t *) count_page_indexes(l_mem);
-	}
-
-	return NULL;
-}
-
 static size_t return_size(LOGICAL_MEMORY *array_instance){
 	size_t array_size;
 	/*
 	 * Below gets the absolute size of an index
 	 */
-	array_size = sizeof((size_t) array_instance->logical_space[PTE_SIZE]) /
-							sizeof((size_t) array_instance->logical_space[0]);
+	array_size = sizeof(array_instance->logical_space[PTE_SIZE]) /
+							sizeof(array_instance->logical_space[0]);
 
 	return array_size;
 }
-static uint32_t **which_free_frame(LOGICAL_MEMORY *ARRAY_INSTANCE)
+
+static LOGICAL_MEMORY **which_free_frame()
 {
-        size_t size = 100;
-	uint32_t **pointer_to_free_frame = malloc(size * sizeof(uint32_t *));
-	for (int index = 0; index < size; index++) {
-		if (ARRAY_INSTANCE->pte_frame[index]!= NULL) {
-		  pointer_to_free_frame[index] = ARRAY_INSTANCE->pte_frame[index];
-		} else {
-			pointer_to_free_frame[index] = NULL;
+	int count = 0;
+	LOGICAL_MEMORY *ARRAY_INSTANCE;
+    LOGICAL_MEMORY **pointer_to_free_frame = malloc(PAGE_NUMBER * sizeof(LOGICAL_MEMORY *));
+	for (int index = 0; index < sizeof(pointer_to_free_frame); index++) {
+		if (ARRAY_INSTANCE->pte_frame[index] == NULL) {
+			pointer_to_free_frame[count++] = NULL; 
 		}
 	}
+	pointer_to_free_frame[count] = NULL;
 	return pointer_to_free_frame;
 }
 
-static uint32_t **allocate_frame(pid_t *process, uint32_t *FRAME_INSTANCE)
+static void allocate_frame(pid_t *process_addr, LOGICAL_MEMORY **FRAME_FREE)
 {
-        size_t s_array = return_size((LOGICAL_MEMORY *)FRAME_INSTANCE);	
-	uint32_t **free_spaces = which_free_frame((LOGICAL_MEMORY *)FRAME_INSTANCE);
-
+        size_t FRAME_SIZE = sizeof(FRAME_FREE);
 	// Allocate a frame inside of multi dimensional array of logical memory
-	for (int frame_populate = 0; s_array < frame_populate; frame_populate++) {
-		if (free_spaces[frame_populate] != NULL) {
-		  ((LOGICAL_MEMORY *)FRAME_INSTANCE)->pte_frame[frame_populate] = (uint32_t *)process;
-		}
-	}
-	return ((LOGICAL_MEMORY *)FRAME_INSTANCE)->pte_frame;
+        for (int frame_index = 0; frame_index < FRAME_SIZE; frame_index++) {
+			FRAME_FREE[frame_index] = (LOGICAL_MEMORY *)process_addr; 
+	}	
 }
 
-static LOGICAL_MEMORY **allocate_mem_logical(LOGICAL_MEMORY *l_instance) {
-	return (LOGICAL_MEMORY**)l_instance->logical_space;
+static void allocate_mem_logical(LOGICAL_MEMORY *logical_instance) {
+	logical_instance->logical_space = malloc(ADDRESS_SPACE * sizeof(LOGICAL_MEMORY *));
 }
 
-static uint32_t **which_free_logical_entry(LOGICAL_MEMORY **entry_instance)
-{
-	size_t size = return_size(*entry_instance);
-	uint32_t **pointer_to_free_logical_entry = malloc(size * sizeof(uint32_t *)); 
-	for (int entry_index = 0; entry_index < size; entry_index++) {
-	  if ((*entry_instance)->logical_space[entry_index] != 0) {
-		  pointer_to_free_logical_entry[entry_index] = malloc(PTE_SIZE);
-		} else {
-			pointer_to_free_logical_entry[entry_index] = NULL; 
-		}
-	}
-	return pointer_to_free_logical_entry;
-}
-
-static uint32_t *DEBUG_ADDR_LOGICAL(LOGICAL_MEMORY *lm)
+static LOGICAL_MEMORY *DEBUG_ADDR_LOGICAL(LOGICAL_MEMORY *lm)
 {
 	return lm->logical_space;
 } 
 
-static void *allocate_entry_logical(LOGICAL_MEMORY **lm_instance, LOGICAL_MEMORY *f_instance, pid_t *process)
-{
-	/*
-	 * TAKES FRAME POINTER AND ADD IT TO LOGICAL MEMORY
-	 */
-	uint32_t **to_free_frame = which_free_frame(f_instance);
-	uint32_t **to_free_logical = which_free_logical_entry(lm_instance);
-
-	if (to_free_logical == NULL) {
-		errno = ENOMEM;
-		return NULL;
-	}
-
-	uint32_t **the_frame = allocate_frame(process, (uint32_t *)f_instance);
-	for (int logical_entry_index = 0; (*lm_instance)->logical_space[logical_entry_index] != 0
-						&& to_free_frame[logical_entry_index] != 0; logical_entry_index++) {
-	  (*lm_instance)->logical_space[logical_entry_index] = *(the_frame[logical_entry_index]);			     	
-	}
-	return NULL;
-}
-
-static uint32_t GET_LOGICAL_ADDRESS(uint32_t PAGE_NUMBER, uint32_t OFFSET)
-{
-	uint32_t logical_addr = (PAGE_NUMBER * PAGE_SIZE) + OFFSET;
-	return logical_addr;
-}
-
-static uint32_t PAGE_NUMBER(pid_t *process)
-{
-  uint32_t get_pn = (*process) / PAGE_SIZE;
-  
-	return get_pn;
-}
-
-static uint32_t OFFSET(uint32_t pn, pid_t *process)
-{
-  uint32_t get_offset = (*process) - (pn * PAGE_SIZE);
-	return get_offset;
-}
-
 int main(int argc, char *argv[])
 {
-	LOGICAL_MEMORY l, *lg, *to_struct;
+    LOGICAL_MEMORY l, *lg;
 
 	/*
 	 * ALLOCATE FOR LOGICAL MEMORY
 	 */
-	LOGICAL_MEMORY *pointer_logical = malloc(sizeof(LOGICAL_MEMORY));;
-	allocate_mem_logical(pointer_logical); // THIS WILL HAVE THE POINTER
+	allocate_mem_logical(lg);
+	LOGICAL_MEMORY **space = &lg->logical_space;
 
 	/*
 	 * ALLOCATE A MEMORY FOR EACH FRAME
 	 */
-	to_struct = malloc(sizeof(LOGICAL_MEMORY));
-        to_struct->pte_frame = malloc(10 * sizeof(uint32_t *));
-	uint32_t **free_frames = which_free_frame(pointer_logical);
+    lg->pte_frame = malloc(PAGE_NUMBER * sizeof(LOGICAL_MEMORY *));
+	LOGICAL_MEMORY **free_frames = which_free_frame();
 
 	/*
 	 * COMMENCE CREATE_PROCESS()
@@ -206,20 +129,18 @@ int main(int argc, char *argv[])
 	/*
 	 * ALLOCATION FOR LOGICAL MEMORY STARTS HERE
 	 */
-	uint32_t **frame_instance = allocate_frame(&pr, (uint32_t *)free_frames);
-	
+    allocate_frame(&pr, free_frames);
+
 	/*
-	 * PROCESS CHECK UP
+	 * PUT FRAME TO SPACE
 	 */
-	if (parent_check(&pr) == PARENT_SUCCESS) {
-	   allocate_entry_logical(&pointer_logical, (LOGICAL_MEMORY *)free_frames, &pr);
-	}
+	lg->logical_space = *lg->pte_frame;
 
 	/*
 	 * PAGE NUMBER & OFFSET
 	 */
-	uint32_t PN = PAGE_NUMBER(&pr);	
-	uint32_t OFT = OFFSET(PN, &pr);
+	uint32_t PN = PAGE_NUMBER;	
+	uint32_t OFT = OFFSET((uint32_t)(uintptr_t)space, PAGE_SIZE);
 
 	/*
 	 * DEBUG OPTION ON 
@@ -227,7 +148,7 @@ int main(int argc, char *argv[])
 	 * A valid page frame should return no trash value
 	 */
 	if(strcmp(argv[1], "-d") == 0) {
-	    if (pointer_logical != NULL && lg != NULL) {
+	    if (space != NULL && lg != NULL) {
 			printf("logical space: ok\nframe: ok") ; 
 		} else {
 			errno = 0;
